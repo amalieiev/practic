ed33064596e05e96b8908526410ab8d5e896b309
artem.malieiev <artem.malieiev@exclaimer.com>
2021-10-25 18:11:42 +0300

    updated example

--- index.js
+++ index.js
@@ -1,73 +1,109 @@
 import { useSubject, useMounted, render, useElement } from "./react.js";
 
-function Counter() {
-    const title = useSubject("Some Title");
-    const count = useSubject(0);
-    const el = useElement();
+render(App, {}, document.getElementById("root"));
 
-    count.subscribe((value) => {
-        el.querySelector(".count").innerHTML = `count: ${value}`;
+export function App() {
+    const el = useElement();
+    const todos = useSubject([]);
+    const search = useSubject("");
+    const filter = useSubject("all");
+
+    search.subscribe(() => {
+        render(
+            TodoList,
+            { todos, search, filter },
+            el.querySelector("#todo-list")
+        );
     });
 
-    title.subscribe((value) => {
-        console.log(value);
-        console.log(title.value);
-        el.querySelector(".title").innerHTML = `title: ${title.value}`;
+    filter.subscribe(() => {
+        render(
+            TodoList,
+            { todos, search, filter },
+            el.querySelector("#todo-list")
+        );
+    });
+
+    todos.subscribe(() => {
+        render(
+            TodoList,
+            { todos, search, filter },
+            el.querySelector("#todo-list")
+        );
     });
 
     useMounted(() => {
-        el.querySelector(".increment").addEventListener("click", () => {
-            count.next(count.value + 1);
-        });
+        fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+            .then((r) => r.json())
+            .then((data) => {
+                todos.next(data);
+            });
 
-        el.querySelector(".decrement").addEventListener("click", () => {
-            count.next(count.value - 1);
-        });
-
-        el.querySelector("input").addEventListener("keyup", (event) => {
-            title.next(event.target.value);
-        });
-
-        render(Counter2, {}, el.querySelector(".item"));
+        el.querySelector('[placeholder="search"]').addEventListener(
+            "keyup",
+            (event) => [search.next(event.target.value)]
+        );
     });
 
     return `
-        <input value="${title.value}" type="text" />
-        <p class="title">title: ${title.value}</p>
-        <p class="count">count: ${count.value}</p>
-        <button class="increment">increment</button>
-        <button class="decrement">decrement</button>
-        <div class="item"></div>
+        <input type="text" >
+        <button>add</button>
+        <br>
+        <input type="text" placeholder="search" >
+
+        <div>
+            <label>
+                <input type="radio" name="showmode" />
+                All
+            </label>
+            <label>
+                <input type="radio" name="showmode" />
+                Active
+            </label>
+            <label>
+                <input type="radio" name="showmode" />
+                Completed
+            </label>
+        </div>
+        <div id="todo-list">Loading...</div>
     `;
 }
 
-function Counter2() {
-    const title = useSubject("Some Onother Title");
-    const count = useSubject(10);
+export function TodoList({ todos, search, filter }) {
     const el = useElement();
 
-    count.subscribe((value) => {
-        el.querySelector(".count").innerHTML = `count: ${value}`;
-    });
-
     useMounted(() => {
-        el.querySelector(".increment").addEventListener("click", () => {
-            count.next(count.value + 1);
-        });
+        const elems = el.querySelectorAll(".todo-item");
 
-        el.querySelector(".decrement").addEventListener("click", () => {
-            count.next(count.value - 1);
-        });
+        todos.value
+            .filter((item) => {
+                if (search.value === "") {
+                    return true;
+                } else {
+                    return item.title.includes(search.value);
+                }
+            })
+            .forEach((todo, index) => {
+                render(TodoItem, todo, elems[index]);
+            });
     });
 
-    return `
-        <input value="${title.value}" type="text" />
-        <p class="title">title: ${title.value}</p>
-        <p class="count">count: ${count.value}</p>
-        <button class="increment">increment</button>
-        <button class="decrement">decrement</button>
-    `;
+    return todos.value
+        .filter((item) => {
+            if (search.value === "") {
+                return true;
+            } else {
+                return item.title.includes(search.value);
+            }
+        })
+        .map((todo) => `<div class="todo-item"></div>`)
+        .join("");
 }
 
-render(Counter, {}, document.getElementById("root"));
-render(Counter, {}, document.getElementById("root-2"));
+export function TodoItem({ id, title, completed }) {
+    return `
+        <input type="checkbox" ${completed ? "checked" : ""} >
+        <label>${title}</label>
+        <button>remove</button>
+    `;
+}
